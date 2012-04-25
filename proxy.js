3f75edea4678573633984c5d4f1fcceda01cecc5
Daniel Schwen <daniel@schwen.de>
2012-04-25 11:57:24 -0600

    capture response

--- proxy.js
+++ proxy.js
@@ -1,13 +1,14 @@
 var http = require('http')
+  , fs = require('fs')
   , crypto = require('crypto')
-  , cache = {}
 ;
 
 http.createServer(function(request, response) {
   var proxy, proxy_request
     , md5 = crypto.createHash('md5')
     , requestdata = JSON.stringify( { m:request.method, u:request.url, h:request.headers } )
-    , hash
+    , hash, cache = { chunks: [] }
+    , i
   ;
 
   md5.update( requestdata );
@@ -15,23 +16,36 @@ http.createServer(function(request, response) {
 
   console.log("hash is",hash);
   console.log(requestdata);
-/*
-  proxy = http.createClient(80, request.headers['host'])
-  proxy_request = proxy.request(request.method, request.url, request.headers);
-  proxy_request.addListener('response', function (proxy_response) {
-    proxy_response.addListener('data', function(chunk) {
-      response.write(chunk, 'binary');
+
+  if( 1 ) {
+    // file is not cached yet
+    proxy = http.createClient(80, request.headers['host'])
+    proxy_request = proxy.request(request.method, request.url, request.headers);
+
+    proxy_request.addListener('response', function (proxy_response) {
+      proxy_response.addListener('data', function(chunk) {
+        cache.chunks.push( chunk.toString('base64') );
+        response.write(chunk, 'binary');
+      });
+      proxy_response.addListener('end', function() {
+        response.end();
+        // save the serialized cache object to disk
+        console.log( JSON.stringify(cache) );
+      });
+      response.writeHead(proxy_response.statusCode, proxy_response.headers);
     });
-    proxy_response.addListener('end', function() {
-      response.end();
+    
+    // oh, that data should probably go into the hash as well
+    request.addListener('data', function(chunk) {
+      proxy_request.write(chunk, 'binary');
     });
-    response.writeHead(proxy_response.statusCode, proxy_response.headers);
-  });
-  request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
-  });
-  request.addListener('end', function() {
-    proxy_request.end();
-  });
-  */
+    request.addListener('end', function() {
+      proxy_request.end();
+    });
+  } else {
+    // retrieve cache object
+    for( i=0; i < cache.chunks.length; ++i ) {
+      response.write( new Buffer(cache.chunks[i],'base64'), 'binary' );
+    }
+  }
 }).listen(13457);
